6bfd820328de3054765586302f4ea9ec194eb1b1
Hokyeong Choi <dollhy@gmail.com>
2020-01-05 22:14:37 +0900

    Remove duplicate festivals

---
+++ manual-tests/duplicates.py
@@ -0,0 +1,44 @@
+# https://stackoverflow.com/questions/17388213/find-the-similarity-metric-between-two-strings
+
+from difflib import SequenceMatcher
+import json
+
+
+WLIST = [33]
+
+def similar(a: str, b: str) -> float:
+    return SequenceMatcher(None, a, b).ratio()
+
+def get_max_date(dates: list) -> (int, int):
+    """
+    dates: [
+        {"year": int, "month": int, "day": int},
+        ...,
+    ]
+    Example:
+    dates: [{'year': 2019, 'month': 12, 'day': 21}, {'year': 2019, 'month': 12, 'day': 29}]"""
+    y = -1
+    m = -1
+    for d in dates:
+        if d["year"] > y:
+            y = d["year"]
+        if d["month"] > m:
+            m = d["month"]
+    return (y, m)
+
+with open("2019.json", encoding="utf-8") as f:
+    lst_of_dicts = json.load(f)
+
+# fail = False
+
+for i in lst_of_dicts:
+    for j in lst_of_dicts:
+        if i["id"] > j["id"] and\
+        get_max_date(i["date"]) == get_max_date(j["date"]) and\
+        similar(i["explanation"], j["explanation"]) > 0.7 and\
+        j["id"] not in WLIST:
+            # fail = True
+            print(similar(i["explanation"], j["explanation"]))
+            print(i)
+            print(j)
+            print()
