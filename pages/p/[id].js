ae2a52bfe309dc48fcd6ff056aaed83288b1dff6
Hokyeong Choi <dollhy@gmail.com>
2020-01-15 22:48:26 +0900

    Shorten url pattern
    Changed url pattern because it was messy and the server was trusting
    browser's input(via url) too much.
    
    I chose searching json file by id and date info over properly indexing
    json file. The reason was that if I pre-sorted json file by id for
    indexing, I would have to sort by date again when rendering festival
    list. and I don't know how to sort in less than n*log(n) time.. But if I
    pre-sort json file by date, when searching festival by id and date, I
    can search through mostly by log(n) time. Considering the size of the
    file(less than 300 festivals) I think it's acceptable for now.
    But it just doesn't matter either way, I just had to change url patterns

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -3,11 +3,14 @@ import Head from 'next/head';
 import fes from '../../2019.json';
 import FullWidthTabs from '../../components/FullWidthTabs';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import queryString from 'query-string';
+import getTime from '../../lib/getTime';
+import ErrorPage from 'next/error';
 
 
-const Info = ({ fe, res }) => {
-
+const Info = ({ fe, res, err }) => {
+  if (err) {
+    return <ErrorPage statusCode={err} />
+  }
 
   const theme = createMuiTheme({
     palette: {
@@ -41,11 +44,57 @@ const Info = ({ fe, res }) => {
 }
 
 Info.getInitialProps = async function (context) {
-  const decodeInPlace = obj => {
-    Object.keys(obj).map(key => obj[key] = decodeURIComponent(obj[key]));
+  const searchLin = (qid) => {
+    for (let festival of fes) {
+      if (festival.id === qid) return festival;
+    }
+    return false;
+  }
+  const searchBin = (qid, qtime, l, r) => {
+    let low = l, high = r;
+    while (low <= high) {
+      const mid = ~~((low + high) / 2); // there's no overflow, ~~ is for floor division
+      const festival = fes[mid];
+      const festivalDate = getTime(festival.date);
+      // console.log(festivalDate);
+      if (festival.id === qid) {
+        return festival;
+      } else if (festivalDate === 0 || festivalDate === qtime) {
+        const res = searchBin(qid, qtime, low, mid - 1);
+        if (res) return res;
+        return searchBin(qid, qtime, mid + 1, high);
+      } else if (festivalDate < qtime) {
+        low = mid + 1;
+      } else { // festivalDate > qtime
+        high = mid - 1;
+      }
+    }
+    return false;
+  }
+
+  // https://stackoverflow.com/questions/26482645/number-isintegerx-which-is-created-can-not-work-in-ie/26482951
+  // Polyfill for Internet Explorer
+  Number.isInteger = Number.isInteger || function(value) {
+    return typeof value === "number" &&
+           isFinite(value) &&
+           ~~(value) === value;
   };
-  let fe = queryString.parse(context.query.id);
-  decodeInPlace(fe);
+
+  const qqid = context.query.id;
+  
+  let [qid, qtime] = qqid.split('&').map(s => Number(s));
+  if (!Number.isInteger(qid) || !Number.isInteger(qtime)) {
+    return {err: 400};
+  }
+
+  let fe;
+  if (qtime === 0) {
+    fe = searchLin(qid);
+  } else {
+    fe = searchBin(qid, qtime, 0, fes.length - 1);
+  }
+  if (!fe) return {err: 404};
+  
   let res;
   try {
     try {
