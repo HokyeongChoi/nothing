50085d0b82f50ebf79e41b2d8e39938515878421
Hokyeong Choi <dollhy@gmail.com>
2020-01-25 10:34:51 +0900

    Remove old link

--- manual-tests/remove_url.py
+++ manual-tests/remove_url.py
@@ -1,7 +1,7 @@
 import json
 
 
-d_list = [58, 86, 241, 243, 250]
+d_list = [58, 86, 241, 243, 250, 193] # 193 서울동화축제 2019
 
 with open("2019.json", encoding="utf-8") as f:
     lst_of_dicts = json.load(f)
@@ -13,5 +13,5 @@ for festival in lst_of_dicts:
         print(f"Removing { i }: { url }")
         festival["link"] = ""
 
-with open("2019n.json", 'w', encoding="utf-8") as f:
+with open("2019.json", 'w', encoding="utf-8") as f:
     f.write(json.dumps(lst_of_dicts, ensure_ascii=False))
